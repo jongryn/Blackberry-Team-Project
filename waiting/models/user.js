e89ca7441c00349bed54a3cad8d7605cbd8c7c90
jayyymorales <jrmorales@ymail.com>
2017-11-18 12:01:21 -0800

    fix

--- waiting/models/user.js
+++ waiting/models/user.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  phonenumber: { type: String, required: true},
+  email: { type: String, required: true},
+  time: { type: Date, default: Date.now }
 });
 
-const Restaurant = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
