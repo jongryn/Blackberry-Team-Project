eef73a144419a4c4de9bd3d0c10ea3b025082f52
Michael Osgood <michaelosgood@dhcp-v120-099.mobile.uci.edu>
2017-12-08 22:19:54 -0800

    updated button styling

--- client/src/components/Form/FormBtn.js
+++ client/src/components/Form/FormBtn.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 export const FormBtn = props =>
-  <button {...props} style={{ float: "center", width: '100%'}} className="btn btn-danger btn-md">
+  <button {...props} style={{ float: "center", width: '100%'}} className="btn btn-md">
     {props.children}
   </button>;
